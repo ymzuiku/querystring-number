eb4d479e28fd84c486a29a6c06f844c639cbf6d5
梁柱 <liangzhu@77ircloud.com>
2019-09-29 00:27:23 +0800

    fix: main name

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,36 @@
 const rollupTypescript = require('rollup-plugin-typescript2');
 const { uglify } = require('rollup-plugin-uglify');
+const path = require('path');
+const fs = require('fs');
+const pwd = (...args) => path.resolve(process.cwd(), ...args);
+
+function rm(file) {
+  const stat = fs.statSync(file);
+  if (stat) {
+    if (stat.isDirectory()) {
+      fs.rmdirSync(file);
+    } else if (stat.isFile()) {
+      fs.unlinkSync(file);
+    }
+  }
+}
+
+function clearDir(dir) {
+  if (fs.existsSync(pwd(dir))) {
+    const files = fs.readdirSync(pwd(dir));
+    files.forEach(file => {
+      rm(pwd(dir, file));
+    });
+  }
+}
+
+clearDir('umd');
 
 module.exports = [
   {
     input: './lib/index.ts',
     output: {
-      file: './umd/index.umd.js',
+      file: './umd/index.js',
       format: 'umd',
       name: 'queryString',
       sourcemap: true,
